bac7450c55c871008d37145e6e45a2066cad1dd0
Gustavo Velasquez <gustavov0706@gmail.com>
2020-06-11 18:15:40 -0300

    Added React Hook to project

--- src/App.js
+++ src/App.js
@@ -1,59 +1,45 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex-centered">
-      {/* puedes alternar entre true y false */}
-      {/* el objetivo es que el switch maneje su propio estado y 
-      no lo lea desde una prop */}
       <Switch />
     </div>
   );
 }
-/**
- * El componente Switch es de tipo funcional
- * Que "clase" de cambios deberias realizar para
- * que este componente pueda manejar su propio estado
- * interno?
- */
-class Switch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      on: true,
-      btnClassName: "toggle-btn"
-    };
-  }
-  handleClick = e => {
+
+const Switch = () => {
+  
+  const [on, setOn] = useState(true);
+  const [btnClassName, setBtnClassName] = useState("toggle-btn");
+
+  const handleChance = e => {
     const btnClassName = [
       "toggle-btn",
-      this.state.on ? "toggle-btn-on" : "toggle-btn-off"
+      on ? "toggle-btn-on" : "toggle-btn-off"
     ]
       .filter(Boolean)
       .join(" ");
-    this.setState({
-      on: !this.state.on,
-      btnClassName
-    });
+    // actualiza estados  
+    setOn(!on);
+    setBtnClassName(btnClassName);
   };
 
-  render() {
-    return (
-      <div>
-        <label style={{ display: "block" }}>
-          {/* Este es el boton como tal, utilizamos un input tipo checkbox */}
-          <input
-            className="toggle-input"
-            type="checkbox"
-            checked={this.state.on}
-            onChange={this.handleClick}
-          />
-          <span className={this.state.btnClassName} />
-        </label>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <label style={{ display: "block" }}>
+        {/* Este es el boton como tal, utilizamos un input tipo checkbox */}
+        <input
+          className="toggle-input"
+          type="checkbox"
+          checked={on}
+          onChange={handleChance}
+        />
+        <span className={btnClassName} />
+      </label>
+    </div>
+  );
 }
 
 export default App;
