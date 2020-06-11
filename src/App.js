5e79a98968884f7b23454e0d12b31d816d0ae46c
Gustavo Velasquez <gustavov0706@gmail.com>
2020-06-11 17:35:01 -0300

    project start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-centered">
+      {/* puedes alternar entre true y false */}
+      {/* el objetivo es que el switch maneje su propio estado y 
+      no lo lea desde una prop */}
+      <Switch on={true} />
+    </div>
+  );
+}
+/**
+ * El componente Switch es de tipo funcional
+ * Que "clase" de cambios deberias realizar para
+ * que este componente pueda manejar su propio estado
+ * interno?
+ */
+
+function Switch(props) {
+  // mecanismo para aplicar las clases de on/off
+  const btnClassName = [
+    "toggle-btn",
+    props.on ? "toggle-btn-on" : "toggle-btn-off"
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <div>
+      <label style={{ display: "block" }}>
+        {/* Este es el boton como tal, utilizamos un input tipo checkbox */}
+        <input className="toggle-input" type="checkbox" checked={props.on} />
+
+        <span className={btnClassName} />
+      </label>
     </div>
   );
 }
